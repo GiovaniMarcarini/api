c897d1b08fa9f884710b3e9d89c1c673577a67cc
Giovani <inf_giovani@hotmail.com>
2022-11-29 18:58:26 -0300

    Initial commit

---
+++ lib/pages/home_page.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+
+  @override
+  State<StatefulWidget> createState() => _HomePageState();
+
+}
+
+class _HomePageState extends State<HomePage> {
+
+  var _fragmentIndex = 0;
+ // final _listaCidadesKey = GlobalKey<ListaCidadesFragmentState>();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        //title: Text(_fragmentIndex == 0
+          //  ? ConsultaCepFragment.title : ListaCidadesFragment.title),
+      ),
+      body: _buildBody(),
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _fragmentIndex,
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.search),
+           // label: ConsultaCepFragment.title,
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.list),
+           // label: ListaCidadesFragment.title,
+          ),
+        ],
+        onTap: (int newIndex) {
+          if (newIndex != _fragmentIndex) {
+            setState(() {
+              _fragmentIndex = newIndex;
+            });
+          }
+        },
+      ),
+      floatingActionButton: _buildFloatingActionButton(),
+    );
+  }
+
+  //Widget _buildBody() => _fragmentIndex == 0
+      //? ConsultaCepFragment() : ListaCidadesFragment(key: _listaCidadesKey);
+
+  Widget? _buildFloatingActionButton() {
+    if (_fragmentIndex == 0) {
+      return null;
+    }
+    return FloatingActionButton(
+      child: const Icon(Icons.add),
+      tooltip: 'Cadastrar Cidade',
+      //onPressed: () => _listaCidadesKey.currentState?.abrirForm(),
+    );
+  }
+
+}
